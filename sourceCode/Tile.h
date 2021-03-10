3c8f9396348a616f1400f1aa939bdcc8d07557fb
Aliyah Davis <56611345+AliMDavis@users.noreply.github.com>
2021-03-10 18:00:20 -0500

    organization of files

---
+++ sourceCode/Tile.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#ifndef TILE_H
+#define TILE_H
+class Tile{
+        public:
+        Tile();
+        bool getTileColor()const;///background color
+        bool is_empty();
+        void set_TileColor(bool isBlack);
+        bool const static BBLACK = true;
+        bool const static BWHITE = false;
+        int const static EMPTY = 0;
+
+        private:
+        bool Tile_color;
+        int piece_state;
+};
+#endif
