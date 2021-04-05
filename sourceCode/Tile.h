a8ff8033d916be7dbc5cbec4f74e2ba2f34abc37
Alyssa Vandale <av117818@ohio.edu>
2021-04-05 14:49:39 -0400

    fixed makefile

--- sourceCode/Tile.h
+++ sourceCode/Tile.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Tile.h
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
@@ -19,34 +18,7 @@ class Tile{
                 bool Tile_color;
                 int piece_state;
 
-                //arrays to hold the letters of the paths on each side
-                char left_side[5];
-                char right_side[5];
-                char upper_side[5];
-                char lower_side[5];
-
                
 
 };
-=======
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#ifndef TILE_H
-#define TILE_H
-class Tile{
-        public:
-        Tile();
-        bool getTileColor()const;///background color
-        bool is_empty();
-        void set_TileColor(bool isBlack);
-        bool const static BBLACK = true;
-        bool const static BWHITE = false;
-        int const static EMPTY = 0;
-
-        private:
-        bool Tile_color;
-        int piece_state;
-};
->>>>>>> master:sourceCode/Tile.h
 #endif
--- sourceCode/Tsuro.h
+++ sourceCode/Tsuro.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Tsuro.h
 #ifndef TSURO_H
 #define TSURO_H
 #include <iostream>
@@ -26,36 +25,6 @@ private:
     Tile board[3][3];
     vector<Tile> IndividualTile;
 
-    //this will be implemented later in the project
-    //it is the vector that will hold all the players moves
-    vector<string> player_moves;
-
-    vector<Tile> tiles;
 };
 
-=======
-#ifndef TSURO_H
-#define TSURO_H
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <string> 
-#include <vector>
-#include "Tile.h"
-#include "game.h"
-using namespace std;
-
-class Tsuro:public game{
-    public:
-    //void display_board();
-    void restart();
-    virtual void display_board( ) const;
-    void transferTile(vector<Tile> IndividualTile);
-
-    private:
-    Tile board[3][3];
-    vector<Tile> IndividualTile;
-};
-
->>>>>>> master:sourceCode/Tsuro.h
 #endif
