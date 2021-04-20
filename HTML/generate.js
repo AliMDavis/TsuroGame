50cfb77115326c21617787705789ffcaa75f293b
AliMDavis <ad767218@ohio.edu>
2021-04-20 06:19:51 -0400

    added to function

--- HTML/generate.js
+++ HTML/generate.js
@@ -1,2 +1,14 @@
+document.getElementById("name").onclick = function() 
+{
+	var name = prompt("Enter number of players" );
+	document.getElementById("out").innerText = name;
 
+	var textToSave = document.getElementById("out").innerHTML;
+	var hiddenElement = document.createElement('a');
+  
+	hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+	hiddenElement.target = '_blank';
+	hiddenElement.download = 'players.txt';
+	hiddenElement.click();
+}
 
